8a9c90eb05bd9d96529ca5313dd21a68b4e595f1
Timothy Cevallos <timcev@gmail.com>
2017-10-06 15:27:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-class BoardMember(name, homeState, training){
+class BoardMember{
+  constructor(name, homeState, training){
   this.name = name;
   this.homeState = homeState;
   this.training = training;
 }
-
-BoardMember.prototype.veto = function(){
-    return "No, I must disagree"
+  veto(){
+    return 'No, I must disagree'
   }
-
-BoardMember.prototype.approve = function(){
-    return "You can do that!"
+  approve(){
+    return 'You can do that!'
   }
-
-BoardMember.prototype.doCharity =function(){
+  doCharity(){
     return "I like to help people."
   }
-
-BoardMember.prototype.releasePressStatement =function(){
+  releasePressStatement(){
     return "You will see great things from Scuber."
   }
-BoardMember.prototype.sayHi = function(){
+  sayHi(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
-
+}
 class Ceo extends BoardMember {
   hireEmployee = fucntion(){
     return "Welcome aboard!"
-}
+  }
 }
